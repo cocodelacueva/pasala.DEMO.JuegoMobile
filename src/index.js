83a1a916d30b2b649963f1b5009fbe6f859a387f
coco <cocodelacueva@gmail.com>
2024-10-25 19:31:00 -0300

    listo para empezar a programar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './assets/css/style.scss';
-//import { smoothScroll } from './assets/js/helpers';
-// import {CarouselInit} from './assets/js/carousel.js'
-//import legalesModal from './assets/js/legales-modal';
-//import animationsCSS from './assets/js/animations-css';
 
 (function init() {
   const siteWrapper = document.querySelector('#site-wrapper');
@@ -12,15 +8,6 @@ import './assets/css/style.scss';
     //do
     console.log('This site is loaded');
 
-    //legales modal
-    //legalesModal(siteWrapper);
-
-    //animations on view:
-    
-    // const wrapperProductos = siteWrapper.querySelector('#productos');
-    // CarouselInit()
-    // let primerSlider = new Carousel(wrapperProductos)
-    // primerSlider.initialization()
 
   } else {
     //reload
