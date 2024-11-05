0389e29fa4408df03cba4383c03d6d46067edba6
coco <cocodelacueva@gmail.com>
2024-11-05 19:44:35 -0300

    error typo

--- src/assets/js/constants.js
+++ src/assets/js/constants.js
@@ -1,6 +1,6 @@
 //valores constantes
 const widthCanvas = 360;
-const heithCanvas = 600;
+const heightCanvas = 600;
 const rows = 20;
 const cols = 10;
 const radiusPosition = 10;
@@ -20,47 +20,47 @@ const formations = {
     '4-4-2': {
         gk: {
             x: widthCanvas / 10 * 5,
-            y: heithCanvas / 20 * 19
+            y: heightCanvas / 20 * 19
         },
         lb: {
             x: widthCanvas / 10 * 1,
-            y: heithCanvas / 20 * 14
+            y: heightCanvas / 20 * 14
         },
         lcb: {
             x: widthCanvas / 10 * 3,
-            y: heithCanvas / 20 * 16
+            y: heightCanvas / 20 * 16
         },
         rcb: {
             x: widthCanvas / 10 * 7,
-            y: heithCanvas / 20 * 16
+            y: heightCanvas / 20 * 16
         },
         rb: {
             x: widthCanvas / 10 * 9,
-            y: heithCanvas / 20 * 14
+            y: heightCanvas / 20 * 14
         },
         lm: {
             x: widthCanvas / 10 * 2,
-            y: heithCanvas / 20 * 10
+            y: heightCanvas / 20 * 10
         },
         lcm: {
             x: widthCanvas / 10 * 4,
-            y: heithCanvas / 20 * 12
+            y: heightCanvas / 20 * 12
         },
         rcm: {
             x: widthCanvas / 10 * 6,
-            y: heithCanvas / 20 * 12
+            y: heightCanvas / 20 * 12
         },
         rm: {
             x: widthCanvas / 10 * 8,
-            y: heithCanvas / 20 * 10
+            y: heightCanvas / 20 * 10
         },
         ls: {
             x: widthCanvas / 10 * 4,
-            y: heithCanvas / 20 * 7
+            y: heightCanvas / 20 * 7
         },
         rs: {
             x: widthCanvas / 10 * 6,
-            y: heithCanvas / 20 * 7
+            y: heightCanvas / 20 * 7
         },
     },
     '4-3-3': {
@@ -78,4 +78,4 @@ const formations = {
 }
 
 
-export { widthCanvas, heithCanvas, colors, formations, rows, cols, radiusPosition, offsetNPC };
+export { widthCanvas, heightCanvas, colors, formations, rows, cols, radiusPosition, offsetNPC };
